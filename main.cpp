346527905699e6b774fbdc6c2deab2a410a61971
Will Thomas <technimanx@gmail.com>
2020-06-14 21:13:05 +0100

    Add method to print help and about text

--- main.cpp
+++ main.cpp
@@ -5,6 +5,74 @@
 #include "Database.hpp"
 #include "Commands.hpp"
 
+void print_help(COMMANDS command = COMMANDS::HELP)
+{
+    // if a command is specified, just print that one
+
+    // print about
+    std::cout << "Miniatures-Tracker v0.1 by Will Thomas <technimanx@gmail.com>" << std::endl;
+    std::cout << "Source at https://github.com/techniman/miniatures-tracker" << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "command <mandatory> [optional]" << std::endl << std::endl;
+
+    // print commands usage and descriptions
+    if (command == COMMANDS::HELP || command == COMMANDS::ADD)
+    {
+        std::cout << "add <name> [quantity=1]" << std::endl;
+        std::cout << "If entry exists, add [quantity]; else, create a new one." << std::endl;
+        std::cout << std::endl;
+    }
+    if (command == COMMANDS::HELP || command == COMMANDS::SUBTRACT)
+    {
+        std::cout << "subtract <name> [quantity=1]" << std::endl;
+        std::cout << "If entry exists, subtract [quantity]. If it would become lower than 0, warns the user." << std::endl;
+        std::cout << std::endl;
+    }
+    if (command == COMMANDS::HELP || command == COMMANDS::RENAME)
+    {
+        std::cout << "rename <currentname> <newname>" << std::endl;
+        std::cout << "If entry exists, changes name to <newname>." << std::endl;
+        std::cout << std::endl;
+    }
+    if (command == COMMANDS::HELP || command == COMMANDS::PAINT)
+    {
+        std::cout << "paint <name> [quantity=1]" << std::endl;
+        std::cout << "If entry exists, add [quantity] to the amount painted; else, create a new one with the quantity and painted." << std::endl;
+        std::cout << std::endl;
+    }
+    if (command == COMMANDS::HELP || command == COMMANDS::SEARCH)
+    {
+        std::cout << "search <text>" << std::endl;
+        std::cout << "Show entries that include text in name." << std::endl;
+        std::cout << std::endl;
+    }
+    if (command == COMMANDS::HELP || command == COMMANDS::LIST)
+    {
+        std::cout << "list" << std::endl;
+        std::cout << "List all existing entries." << std::endl;
+        std::cout << std::endl;
+    }
+    if (command == COMMANDS::HELP || command == COMMANDS::HIDE)
+    {
+        std::cout << "hide <name>" << std::endl;
+        std::cout << "Hide an entry." << std::endl;
+        std::cout << std::endl;
+    }
+    if (command == COMMANDS::HELP || command == COMMANDS::RESTORE)
+    {
+        std::cout << "restore <name>" << std::endl;
+        std::cout << "Unhide an entry." << std::endl;
+        std::cout << std::endl;
+    }
+    if (command == COMMANDS::HELP)
+    {
+        std::cout << "help" << std::endl;
+        std::cout << "Shows this!" << std::endl;
+        std::cout << std::endl;
+    }
+}
+
 int main(int argc, char ** args)
 {
     // read the command and args
@@ -69,10 +137,7 @@ int main(int argc, char ** args)
 
     case COMMANDS::HELP:
     default:
-        // print about
-        std::cout << "Miniatures-Tracker by Will Thomas <technimanx@gmail.com>" << std::endl;
-        std::cout << "https://github.com/techniman/miniatures-tracker" << std::endl;
-        // print commands usage and descriptions
+        print_help();
         break;
     }
 
